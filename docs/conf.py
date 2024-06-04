52d81abbb8b6db5c3851d68a46f266f9bc706496
ikaneng-thekiso <119665806+ikaneng-thekiso@users.noreply.github.com>
2024-06-04 10:08:20 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,24 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information -----------------------------------------------------
+
+project = 'Your Project Name'
+copyright = '2021, Your Name'
+author = 'Your Name'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
 
